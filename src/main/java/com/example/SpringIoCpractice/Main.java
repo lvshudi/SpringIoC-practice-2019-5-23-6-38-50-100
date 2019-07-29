58111170a1e53e0807afc7fc7c7273ff659c1e4a
lvshudi <lvshudi119@126.com>
2019-07-29 20:08:56 +0800

    change

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,16 @@
-//TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main{
+	public static void main(String[] args) {
+        
+        // 创建 Spring 的 IOC 容器
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("hello.xml");
+        // 使用 getBean() 方法, 通过传入刚才的 id 名,来获取 bean, 但是这里返回的是一个 Object 对象, 所以要转型
+        Person person = (Person) ctx.getBean("person");
+        // 打印 person
+        System.out.println(person);
+	}
+}
---
+++ src/main/java/com/example/SpringIoCpractice/Person.java
@@ -0,0 +1,18 @@
+package com.example.SpringIoCpractice;
+
+public class Person {
+	String name = "";
+	String sex = "";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
